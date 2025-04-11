82f4aeb40192f3f781ec4e5c6a9a06d6280f164a
Tsaryk Mykola <90026719+TsNikolay@users.noreply.github.com>
2025-04-11 21:26:41 +0200

    feat: Made navbar responsive

--- frontend/src/components/Headers/MainHeader/Header.js
+++ frontend/src/components/Headers/MainHeader/Header.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./Header.module.css";
-
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 import ThemeSwitcher from "../../ThemeSwitcher/ThemeSwitcher";
 import LanguageSwitcher from "../../LanguageSwitcher/LanguageSwitcher";
-import { Link } from "react-router-dom";
 
 const Header = () => {
+  //Наступні дві константи для респонсів навбару
+  const navRef = useRef();
+  const toggleNavbar = () => {
+    navRef.current.classList.toggle(styles.responsiveNav);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>WAY.UA</div>
 
-      <nav className={styles.nav}>
+      <nav ref={navRef} className={styles.nav}>
         <Link to="#">HOME</Link>
-        <Link to="#">ABOUT US</Link>
+        <Link to="#">ABOUT</Link>
         <Link to="#">CONTACTS</Link>
+
+        <div className={styles.mobileControls}>
+          <ThemeSwitcher />
+          <LanguageSwitcher />
+        </div>
+
+        <button
+          className={`${styles.navBtn} ${styles.closeBtn}`}
+          onClick={toggleNavbar}
+        >
+          <FaTimes />
+        </button>
       </nav>
 
       <div className={styles.controls}>
         <ThemeSwitcher />
         <LanguageSwitcher />
         <button className={styles.loginBtn}>LOGIN</button>
+        <button className={styles.navBtn} onClick={toggleNavbar}>
+          <FaBars />
+        </button>
       </div>
     </header>
   );
